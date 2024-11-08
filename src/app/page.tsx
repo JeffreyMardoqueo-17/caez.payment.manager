1680574fcb8f31f99dcf22074053f4df25fe6f8c
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-11-08 12:59:47 -0600

    Login listo ✔

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-// src/app/page.tsx
 "use client";
 
 import React, { useState } from 'react';
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 interface LoginData {
   Login: string;
@@ -13,11 +14,15 @@ interface LoginData {
 const LoginPage: React.FC = () => {
   const [loginData, setLoginData] = useState<LoginData>({ Login: '', Password: '' });
   const [error, setError] = useState<string | null>(null);
+  const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setLoginData({ ...loginData, [name]: value });
+    setLoginData({
+      ...loginData,
+      [name]: name === "Login" ? value.toLowerCase() : value // Convierte solo el campo "Login" a minúsculas
+    });
   };
 
   const handleLogin = async (e: React.FormEvent) => {
@@ -51,30 +56,77 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h2 className="text-2xl font-bold mb-4">Iniciar sesión</h2>
-      <form onSubmit={handleLogin} className="flex flex-col space-y-4">
-        <input
-          type="text"
-          name="Login"
-          placeholder="Correo electrónico"
-          value={loginData.Login}
-          onChange={handleChange}
-          className="p-2 border rounded"
-        />
-        <input
-          type="password"
-          name="Password"
-          placeholder="Contraseña"
-          value={loginData.Password}
-          onChange={handleChange}
-          className="p-2 border rounded"
-        />
-        {error && <p className="text-red-500">{error}</p>}
-        <button type="submit" className="p-2 bg-blue-500 text-white rounded">
-          Iniciar sesión
-        </button>
-      </form>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-2">
+      <div className="w-full max-w-4xl bg-white rounded-lg shadow-xl flex overflow-hidden">
+
+        {/* Sección lateral con fondo degradado */}
+        <div className="hidden md:flex w-1/2 bg-bgAzul flex-col justify-center items-center p-8">
+          <Image
+            src="/path/to/your/logo.png" // Reemplaza con la ruta de tu logo
+            alt="Logo"
+            width={100}
+            height={100}
+            className="mb-6"
+          />
+          <h2 className="text-white text-2xl font-semibold text-center">Bienvenido a Administración CAEZ</h2>
+        </div>
+
+        {/* Formulario de inicio de sesión */}
+        <div className="w-full md:w-1/2 p-10 flex flex-col justify-center">
+          <h2 className="text-2xl font-bold text-bgAzul text-center mb-6">Iniciar sesión</h2>
+
+          <form onSubmit={handleLogin} className="flex flex-col space-y-4">
+            <div className="relative">
+              <input
+                type="email"
+                name="Login"
+                placeholder="Correo electrónico"
+                value={loginData.Login}
+                onChange={handleChange}
+                className="w-full p-3 bg-gray-50 rounded-xl border border-gray-100 focus:border-blue-700 focus:outline-none text-gray-700"
+                required
+                onContextMenu={(e) => e.preventDefault()} // Deshabilita inspección del campo
+              />
+            </div>
+
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"} // Cambia entre 'text' y 'password' según el estado
+                name="Password"
+                placeholder="Contraseña"
+                value={loginData.Password}
+                onChange={handleChange}
+                className="w-full p-3 bg-gray-50 rounded-xl border border-gray-100 focus:border-blue-700 focus:outline-none text-gray-700"
+                required
+                onContextMenu={(e) => e.preventDefault()} // Deshabilita inspección del campo
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-0 right-0 px-3 flex items-center text-gray-500"
+                tabIndex={-1} // Evita que el botón se seleccione al navegar con el tab
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+
+            {error && <p className="text-red-500 text-center">{error}</p>}
+
+            <button
+              type="submit"
+              className="w-full py-3 bg-bgAzul text-white rounded-md font-semibold hover:bg-blue-950 transition-all duration-300"
+            >
+              Ingresar
+            </button>
+          </form>
+
+          <div className="mt-6 text-center">
+            <a href="#" className="text-blue-600 text-sm hover:underline">
+              ¿Olvidaste tu contraseña?
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
